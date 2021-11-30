1f83886f0c975e4202e164df6cadbc836b7b71e2
Vladimir Trofimov <trofimov57@mail.ru>
2021-11-30 23:50:35 +0200

    Camera moves

--- Multiplayer FPS/Assets/Scripts/PlayerController.cs
+++ Multiplayer FPS/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Camera
+    /// </summary>
+    public Transform viewPoint;
+
+    public float mouseSensitivity = 1f;
+    private float verticalRotationStore;
+    private Vector2 mouseInput;
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
         
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y 
+                                                                + mouseInput.x, transform.rotation.eulerAngles.z);
     }
 }
